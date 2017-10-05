c177a81fb6969012cd6fcc9c9000ffda5c1f8267
murosan <masashi_16n1200013@nnn.ed.jp>
2017-10-05 15:42:54 +0900

    初級

--- src/main/scala/MustResume.scala
+++ src/main/scala/MustResume.scala
@@ -1,6 +1,14 @@
-import akka.actor.{Actor, Props}
+import akka.actor.SupervisorStrategy.Resume
+import akka.actor.{Actor, ActorRef, ActorSystem, Inbox, OneForOneStrategy, Props}
+
+import scala.concurrent.duration._
 
 class ParentActor extends Actor {
+  override val supervisorStrategy: OneForOneStrategy =
+    OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1.minutes) {
+      case _: Exception => Resume
+    }
+
   def receive = {
     case p: Props => sender() ! context.actorOf(p)
   }
@@ -14,5 +22,27 @@ class ChildActor extends Actor {
 }
 
 object MustResume extends App {
+  val system = ActorSystem("mustResume")
+  val inbox = Inbox.create(system)
+  implicit val sender: ActorRef = inbox.getRef()
 
+  val parent = system.actorOf(Props[ParentActor], "parent")
+
+  parent ! Props[ChildActor]
+  val child = inbox.receive(5.seconds).asInstanceOf[ActorRef]
+
+  child ! new ArithmeticException("Purposely ArithmeticException")
+  child ! "after ArithmeticException"
+  child ! new NullPointerException("Purposely NullException")
+  child ! "after NullPointerException"
+  child ! new IllegalArgumentException("Purposely IllegalArgument")
+  child ! "after IllegalArgumentException"
+  child ! new Exception("Purposely Exception")
+  child ! "after Exception"
+
+  Thread.sleep(5000)
+  system.terminate()
+
+  // Awaitだと最後まで実行してくれませんでした。
+  // Await.ready(system.terminate(), Duration.Inf)
 }
