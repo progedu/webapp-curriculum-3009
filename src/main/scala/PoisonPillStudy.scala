bf91d4d43fbe73ceef09f76aa14cc30919fe3f33
Yosuke Ogata <ogata0227@outlook.com>
2019-12-15 11:41:26 +0900

    練習問題

--- src/main/scala/PoisonPillStudy.scala
+++ src/main/scala/PoisonPillStudy.scala
@@ -1,3 +1,19 @@
-object PoisonPillStudy extends App {
+import akka.actor.{ActorRef, PoisonPill,ActorSystem, Inbox, Props}
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+object PoisonPillStudy extends App {
+  val system = ActorSystem("poisonPillStudy")
+  val myActor = system.actorOf(Props[ParentActor],"myActor")
+  val inbox = Inbox.create(system)
+  implicit val sender = inbox.getRef()
+
+  myActor ! Props[ChildActor]
+  val childActor = inbox.receive(5.seconds).asInstanceOf[ActorRef]
+
+  childActor ! PoisonPill
+  childActor ! "3" //String
+
+  Thread.currentThread().join()
 }
